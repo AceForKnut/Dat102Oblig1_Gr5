9f2a63a8c114131e910b74f01c00ef19b354247f
798082 <798082@stud.hvl.no>
2025-01-21 13:50:37 +0100

    Enum, Film begynt

--- Oblig1_Gr5/src/no/hvl/data102/filmarkiv/impl/Film.java
+++ Oblig1_Gr5/src/no/hvl/data102/filmarkiv/impl/Film.java
@@ -1,4 +1,22 @@
 package no.hvl.data102.filmarkiv.impl;
 
 public class Film {
+
+    private int filmNr;
+    private String produsent;
+    private String tittel;
+    private int aar;
+    //enum Sjanger;
+    private String filmSelskap;
 }
+    public Film(int filmNr, String produsent, String tittel, int aar, enum Sjanger, String filmSelskap){
+        this.filmNr = filmNr;
+        this.produsent = produsent;
+        this.tittel = tittel;
+        this.aar = aar;
+        //placeholder for enum
+        this.filmSelskap = filmSelskap;
+    }
+
+
+
