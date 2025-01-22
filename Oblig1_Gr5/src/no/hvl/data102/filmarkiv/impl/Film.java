075fa38b35c6f1d532ffec373aaea7ac56129774
798082 <798082@stud.hvl.no>
2025-01-22 13:51:23 +0100

    Film Ferdig? Filmarkiv påbegynt

--- Oblig1_Gr5/src/no/hvl/data102/filmarkiv/impl/Film.java
+++ Oblig1_Gr5/src/no/hvl/data102/filmarkiv/impl/Film.java
@@ -1,22 +1,99 @@
 package no.hvl.data102.filmarkiv.impl;
 
+import java.util.Objects;
+
 public class Film {
 
     private int filmNr;
     private String produsent;
     private String tittel;
     private int aar;
-    //enum Sjanger;
+    private Sjanger sjanger;
     private String filmSelskap;
-}
-    public Film(int filmNr, String produsent, String tittel, int aar, enum Sjanger, String filmSelskap){
+
+
+    public Film(){
+        this.filmNr = 0;
+        this.produsent = "";
+        this.tittel = "";
+        this.aar = 0;
+        this.sjanger = sjanger.UDEFINERT;
+        this.filmSelskap = "";
+    }
+
+
+    public Film(int filmNr, String produsent, String tittel, int aar, Sjanger sjanger, String filmSelskap){
         this.filmNr = filmNr;
         this.produsent = produsent;
         this.tittel = tittel;
         this.aar = aar;
-        //placeholder for enum
+        this.sjanger = sjanger;
         this.filmSelskap = filmSelskap;
     }
 
+    
+    
+    public int getFilmNr() {
+        return filmNr;
+    }
 
+    public void setFilmNr(int filmNr) {
+        this.filmNr = filmNr;
+    }
 
+    public String getProdusent() {
+        return produsent;
+    }
+
+    public void setProdusent(String produsent) {
+        this.produsent = produsent;
+    }
+
+    public String getTittel() {
+        return tittel;
+    }
+
+    public void setTittel(String tittel) {
+        this.tittel = tittel;
+    }
+
+    public int getAar() {
+        return aar;
+    }
+
+    public void setAar(int aar) {
+        this.aar = aar;
+    }
+
+    public Sjanger getSjanger() {
+        return sjanger;
+    }
+
+    public void setSjanger(Sjanger sjanger) {
+        this.sjanger = sjanger;
+    }
+
+    public String getFilmSelskap() {
+        return filmSelskap;
+    }
+
+    public void setFilmSelskap(String filmSelskap) {
+        this.filmSelskap = filmSelskap;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; 
+        if (obj == null || getClass() != obj.getClass()) return false; 
+
+        Film film = (Film) obj; 
+        return filmNr == film.filmNr; 
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(filmNr);
+    }
+
+}
+    
--- Oblig1_Gr5/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
+++ Oblig1_Gr5/src/no/hvl/data102/filmarkiv/impl/Filmarkiv.java
@@ -1,4 +1,99 @@
 package no.hvl.data102.filmarkiv.impl;
 
-public class Filmarkiv {
+import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
+
+public class Filmarkiv implements FilmarkivADT {
+
+    private Film[] filmer;
+    private int antall;
+
+    public Filmarkiv(int lengde){
+        this.filmer = new Film[lengde];
+        this.antall = 0;
+    }
+
 }
+    @Override
+    public Film finnFilm(int nr) {
+        for (int i = 0; i < antall; i++){
+            if(filmer[i].getFilmNr() == nr){
+                return filmer[i];
+            } 
+            
+            }
+            return null; //returnerer null ETTER den har gått gjennom hele driten
+        } 
+        
+
+
+    @Override
+    public void leggTilFilm(Film nyFilm) {
+        if (antall == filmer.length){
+            utvidArkiv();
+        }
+        filmer[antall] = nyFilm;
+        antall++;
+        
+    }
+    private void utvidArkiv() {
+        Film[] nyFilmer = new Film[filmer.length * 2]; 
+        for (int i = 0; i < filmer.length; i++) {
+            nyFilmer[i] = filmer[i]; 
+        }
+        filmer = nyFilmer; 
+    }
+
+
+    @Override
+public boolean slettFilm(int filmnr) {
+    for (int i = 0; i < antall; i++) {
+        if (filmer[i] != null && filmer[i].getFilmNr() == filmnr) {
+            // Midlertidig lagrer den siste filmen i arkivet
+            Film temp = filmer[antall - 1];
+
+            // Flytter siste filmen til posisjonen som skal slettes
+            filmer[i] = temp;
+
+            // Nullstiller den siste posisjonen
+            filmer[antall - 1] = null;
+
+            // Reduserer antall gyldige filmer i arkivet
+            antall--;
+
+            return true; // Returnerer true når filmen er slettet
+        }
+    }
+    return false; // Returnerer false hvis filmen ikke finnes i arkivet
+}
+
+    @Override
+    public Film[] soekTittel(String delstreng) {
+
+    }
+
+
+    @Override
+    public Film[] soekProdusent(String delstreng) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'soekProdusent'");
+    }
+
+
+    @Override
+    public int antall(Sjanger sjanger) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'antall'");
+    }
+
+
+    @Override
+    public int antall() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'antall'");
+    }
+
+
+}
+    
+
+
