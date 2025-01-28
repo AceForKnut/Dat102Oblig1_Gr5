c588a14686709819e5016ee65a48d2dad2b2d414
h597236 <597236@stud.hvl.no>
2025-01-28 13:50:42 +0100

    Start på oppg2 update

--- Oblig1_Gr5/src/no/hvl/data102/filmarkiv/impl/Filmarkiv2.java
+++ Oblig1_Gr5/src/no/hvl/data102/filmarkiv/impl/Filmarkiv2.java
@@ -2,6 +2,8 @@ package no.hvl.data102.filmarkiv.impl;
 
 import no.hvl.data102.filmarkiv.adt.FilmarkivADT;
 
+import java.util.Arrays;
+
 public class Filmarkiv2 implements FilmarkivADT {
     private int antall;
     private LinearNode<Film> start;
@@ -15,7 +17,7 @@ public class Filmarkiv2 implements FilmarkivADT {
     public Film finnFilm(int nr) {
         LinearNode<Film> aktuell = start;
         while (aktuell != null) {
-            if(aktuell.data.getFilmNr() == nr) {
+            if (aktuell.data.getFilmNr() == nr) {
                 return aktuell.data;
             }
             aktuell = aktuell.neste;
@@ -55,7 +57,27 @@ public class Filmarkiv2 implements FilmarkivADT {
 
     @Override
     public Film[] soekTittel(String delstreng) {
-        return new Film[0];
+
+        Film[] matchFilm = new Film[antall]; //Lager et nytt arkiv med filmer som matcher delstrengen
+        LinearNode<Film> aktuell = start;
+        int index = 0;
+
+        while (aktuell != null) {
+            if (delstreng.contains(aktuell.data.getTittel())) {
+                matchFilm[index++] = aktuell.data;
+            }
+        }
+           /* if(filmer[i].getTittel().toLowerCase().contains(delstreng.toLowerCase())){
+                matchFilm[index++] = filmer[i];
+            }*/
+
+        return trimArkiv(matchFilm, index);
+    }
+
+
+    //Hjelpe metode for å trimme arkivet ned til bare lengden av matchet søk
+    public Film[] trimArkiv(Film[] arr, int lengde){
+        return Arrays.copyOf(arr, lengde);
     }
 
     @Override
