2ff41f401651cbc6b10b16a2e32e0200686e8bd6
h597236 <597236@stud.hvl.no>
2025-01-28 11:53:36 +0100

    La til "Legg til"

--- Oblig1_Gr5/src/no/hvl/data102/filmarkiv/klient/Meny.java
+++ Oblig1_Gr5/src/no/hvl/data102/filmarkiv/klient/Meny.java
@@ -38,6 +38,62 @@ public class Meny {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
 
+        // Felt for input
+        JLabel idLabel = new JLabel("Film ID:");
+        idLabel.setBounds(50, 30, 100, 20); // Posisjon x, y, bredde, høyde
+
+        JTextField idField = new JTextField();
+        idField.setBounds(150, 30, 200, 20);
+
+        JLabel tittelLabel = new JLabel("Tittel:");
+        tittelLabel.setBounds(50, 60, 100, 20);
+        JTextField tittelField = new JTextField();
+        tittelField.setBounds(150, 60, 200, 20);
+
+        JLabel aarLabel = new JLabel("År:");
+        aarLabel.setBounds(50, 90, 100, 20);
+        JTextField aarField = new JTextField();
+        aarField.setBounds(150, 90, 200, 20);
+
+        JLabel sjangerLabelNew = new JLabel("Sjanger:");
+        sjangerLabelNew.setBounds(50, 120, 100, 20);
+        JComboBox<Sjanger> sjangerComboNew = new JComboBox<>(Sjanger.values());
+        sjangerComboNew.setBounds(150, 120, 200, 20);
+
+        JLabel companyLabel = new JLabel("Produksjonsselskap:");
+        companyLabel.setBounds(50, 150, 150, 20);
+        JTextField companyField = new JTextField();
+        companyField.setBounds(200, 150, 200, 20);
+
+        // Legg til knapp for å legge til film
+        JButton addButton = new JButton("Legg til Film");
+        addButton.setBounds(150, 200, 150, 30);
+        addButton.addActionListener(new ActionListener() { //adder en knapp som kjører når klikket
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int id = Integer.parseInt(idField.getText());
+                    String tittel = tittelField.getText();
+                    int aar = Integer.parseInt(aarField.getText());
+                    Sjanger sjanger = (Sjanger) sjangerComboNew.getSelectedItem();
+                    String company = companyField.getText();
+
+                    // Legg til film i arkivet
+                    filmarkiv.leggTilFilm(new Film(id, "Ukjent", tittel, aar, sjanger, company));
+
+                    JOptionPane.showMessageDialog(frame, "Film lagt til!", "Suksess", JOptionPane.INFORMATION_MESSAGE);
+
+                    // Tøm feltene
+                    idField.setText("");
+                    tittelField.setText("");
+                    aarField.setText("");
+                    companyField.setText("");
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(frame, "Feil input! Sjekk ID og År.", "Feil", JOptionPane.ERROR_MESSAGE); //Om det er input feil i feltene
+                }
+            }
+        });
+
         // Søk etter tittel
         JLabel finnFilmLabel = new JLabel("Søk etter tittel:");
         finnFilmLabel.setBounds(50, 30, 150, 20);
